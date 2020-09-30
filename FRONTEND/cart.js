03fa1c42e791ba92aa112f52536c9d352aa41ab3
iannakim <dear.hyunji@gmail.com>
2020-09-30 18:37:25 -0400

    cart displays items sucessfully

--- FRONTEND/cart.js
+++ FRONTEND/cart.js
@@ -6,8 +6,6 @@ const mainCartContainer = document.querySelector("div.cart-holder")
 
 let currentCart;
 
-    let productQuantity = parseInt(valueSelectedFromQuantity)
-
 let checkIfCartExists = (user) => {
 
     let userLoggingIn = user
@@ -60,7 +58,7 @@ let createCartForUser = (user) => {
 // ---------------------------------------------------------------------------
 
 
-console.log(mainCartContainer)
+// console.log(mainCartContainer)
 
 //  Once I clicked Cart nav render page gets called 
 //  next I will be appending stuff in the DOM to show only on the cart nav page. 
@@ -84,122 +82,112 @@ console.log(mainCartContainer)
 
 
 let renderCartPage = () => {
+    mainBody.innerText = ''
+
 
     console.log("Hey this is coming from renderCartPage at cart.js")
 
-    currentCart.add_products.forEach((product) => {
-        product.forEach((product) => {
-            console.log(product.product.name)
+    fetch(`http://localhost:3000/carts/${currentCart.id}`)
+        .then(res => res.json())
+        .then(cart => {
+            cart.add_products.forEach((itemInCart)=>{
+                displayItemsInCart(itemInCart)
+            })
         })
-    })
-    
 
-    // mainBody.innerText = ''
-
-    // fetch(`http://localhost:3000/carts/${currentCart.id}`)
-    // .then(res => res.json())
-    // .then(cart => {
-    //     console.log(cart)
-    // })
-
-    
+}   
 
 
-    // let basketTitle = document.createElement('h5')
-    //     basketTitle.clasName = 'basket-title'
-    //     basketTitle.innerText = 'My Basket'
-
-    // let horizonLine = document.createElement('div')
-    //     horizonLine.id = 'horizontal-line'
 
 
-    // arrayofProduct.forEach((product) => {
+    let basketTitle = document.createElement('h5')
+        basketTitle.clasName = 'basket-title'
+        basketTitle.innerText = 'My Basket'
 
-//         let cardDeck = document.createElement('div')
-//             cardDeck.className = 'card-deck'
+    let horizonLine = document.createElement('div')
+        horizonLine.id = 'horizontal-line'
 
-//         let productList = document.createElement('div')
-//             productList.className = 'product-list'
+    let cardDeck = document.createElement('div')
+        cardDeck.className = 'card-deck'
 
-//         let singularProduct = document.createElement('div')
-//             singularProduct.className = 'singular-product'
+    let productList = document.createElement('div')
+        productList.className = 'product-list'
 
-
-//         // -----Holder for each product, this card is the looping creation ------
-//         let cardGroup = document.createElement('div')
-//             cardGroup.className = 'card-group'
-//             cardGroup.id = 'product-info'
-
-//         let cardImg = document.createElement('div')
-//             cardImg.className = 'card'
-
-//             // ---Slaping information into the DOM-------
-//         let imgTag = document.createElement('img')
-//             // imgTag.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRcGLtvwxLa0Ns4AudjBgp3LNSM-kpJ9yvh3K4jpcWjXX2y-NEgvB-oyX19dKLM_Rzt2R4Te1g&usqp=CAc'
-//             imgTag.src = product.image
+    let singularProduct = document.createElement('div')
+        singularProduct.className = 'singular-product'
 
 
-//         let cardProdNameQuantity = document.createElement('div')
-//             cardProdNameQuantity.className = 'card'
+  //---Slaping information into the DOM-------
+
+  //this can be a function.
+
+  let displayItemsInCart = (item) => {
+        let cardGroup = document.createElement('div')
+            cardGroup.className = 'card-group'
+            cardGroup.id = 'product-info'
+
+        let cardImg = document.createElement('div')
+            cardImg.className = 'card'
+
+
+        let imgTag = document.createElement('img')
+            // imgTag.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRcGLtvwxLa0Ns4AudjBgp3LNSM-kpJ9yvh3K4jpcWjXX2y-NEgvB-oyX19dKLM_Rzt2R4Te1g&usqp=CAc'
+            imgTag.src = item.product.image
+
+
+        let cardProdNameQuantity = document.createElement('div')
+            cardProdNameQuantity.className = 'card'
         
-//         let productName = document.createElement('p')
-//             productName.id = 'product-name'
-//             // productName.innerText = 'I am Yoda'
-//             productName.innerText = product.name
+        let productName = document.createElement('p')
+            productName.id = 'product-name'
+            // productName.innerText = 'I am Yoda'
+            productName.innerText = item.product.name
 
 
 
-//         let productQuantity = document.createElement('p')
-//             productQuantity.id = 'product-quantity'
-//             productQuantity.innerText = 'qty: 1'
+        let productQuantity = document.createElement('p')
+            productQuantity.id = 'product-quantity'
+            productQuantity.innerText = `Qty: ${item.quantity}`
 
-//         let cardProductPriceRemove = document.createElement('div')
-//             cardProductPriceRemove.className = 'card'
+        let cardProductPriceRemove = document.createElement('div')
+            cardProductPriceRemove.className = 'card'
 
-//         let productPrice = document.createElement('p')
-//             productPrice.id = 'product-price'
-//             // productPrice.innerText = '$45.87'
-//             productPrice.innerText = product.price
+        let productPrice = document.createElement('p')
+            productPrice.id = 'product-price'
+            // productPrice.innerText = '$45.87'
+            productPrice.innerText = `Price: ${item.product.price}`
 
 
-//         let buttonRemove = document.createElement('button')
-//             buttonRemove.id = 'product-remove'
-//             buttonRemove.innerText = 'Remove'
+        let buttonRemove = document.createElement('button')
+            buttonRemove.id = 'product-remove'
+            buttonRemove.innerText = 'Remove'
         
-//         let totalInfo = document.createElement('div')
-//             totalInfo.className = 'total-info'
+        let totalInfo = document.createElement('div')
+            totalInfo.className = 'total-info'
 
-//         let subtotal = document.createElement('p')
-//             subtotal.id = 'subtotal'
-//             subtotal.innerText = "Merchandise Subtotal $ --.00"
+        let subtotal = document.createElement('p')
+            subtotal.id = 'subtotal'
+            subtotal.innerText = "Merchandise Subtotal $ --.00"
 
-//         let estimatedTotal = document.createElement('p')
-//             estimatedTotal.id = 'estimated-total'
-//             estimatedTotal.innerText = 'Estimated Total: $--.00'
+        let estimatedTotal = document.createElement('p')
+            estimatedTotal.id = 'estimated-total'
+            estimatedTotal.innerText = 'Estimated Total: $--.00'
 
-//         let checkOut = document.createElement('button')
-//             checkOut.id = 'check-out'
-//             checkOut.innerText = 'CHECK OUT'
-
-
-//         totalInfo.append(subtotal, estimatedTotal, checkOut)
-//         cardProductPriceRemove.append(productPrice, buttonRemove)
-//         cardProdNameQuantity.append(productName, productQuantity)
-//         cardImg.append(imgTag)
-//         cardGroup.append(cardImg, cardProdNameQuantity, cardProductPriceRemove)
-//         singularProduct.append(cardGroup)
-//         productList.append(singularProduct)
-//         cardDeck.append(productList, totalInfo)
-//         mainBody.append(basketTitle, horizonLine, cardDeck)
+        let checkOut = document.createElement('button')
+            checkOut.id = 'check-out'
+            checkOut.innerText = 'CHECK OUT'
 
 
-//     // })
-
-
-    
-// }
-
-
+        totalInfo.append(subtotal, estimatedTotal, checkOut)
+        cardProductPriceRemove.append(productPrice, buttonRemove)
+        cardProdNameQuantity.append(productName, productQuantity)
+        cardImg.append(imgTag)
+        cardGroup.append(cardImg, cardProdNameQuantity, cardProductPriceRemove)
+        singularProduct.append(cardGroup)
+        productList.append(singularProduct)
+        cardDeck.append(productList, totalInfo)
+        mainBody.append(basketTitle, horizonLine, cardDeck)
+        }
 
 
 
@@ -213,5 +201,5 @@ let renderCartPage = () => {
 
 
 
-}
+
 
